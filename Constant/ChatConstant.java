ae439fba320128718a1f7afdfbe454b13602c2fc
WANG XUAN <AResearcherW@gmail.com>
2023-05-22 17:51:52 +0800

    Add files via upload

---
+++ Constant/ChatConstant.java
@@ -0,0 +1,14 @@
+package contant;
+
+/**
+*@description: 常量类，存放端口号，服务端ip等信息
+*/
+public class ChatConstant {
+
+    public static final int serverPort = 4444;
+
+    public static final String serverIP = "localhost";
+
+    private ChatConstant(){}
+
+}
